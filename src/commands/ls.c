1652d13aa519c6deeee671a0231b8d4e09902d8c
pioupia <pioupiadeux@gmail.com>
2023-02-04 11:47:55 +0100

    Feat: updating ls command

--- src/commands/ls.c
+++ src/commands/ls.c
@@ -4,8 +4,24 @@
 #include <stdio.h>
 #include <dirent.h>
 #include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 extern char **environ;
+
+
+int is_regular_file(const char *path)
+{
+    struct stat path_stat;
+    stat(path, &path_stat);
+    return (S_ISREG(path_stat.st_mode));
+}
+
+int cmpstringp(const void *p1, const void *p2)
+{
+    return strcmp(*(const char **) p1, *(const char **) p2);
+}
 
 int main(int argc, char **argv) {
     if (environ[0] == NULL) return (1);
@@ -27,6 +43,8 @@ int main(int argc, char **argv) {
         }
     }
 
+    printf("%d\n", has_all);
+
     struct dirent *de;
   
     DIR *dr = opendir(environ[0]);
@@ -37,15 +55,39 @@ int main(int argc, char **argv) {
         return (1);
     }
   
+    int files_len = 0;
+    while ((de = readdir(dr)) != NULL) {
+        files_len++;
+    }
+
+    closedir(dr);
+
+    dr = opendir(environ[0]);
+
+    char **files = calloc(files_len, sizeof(char *));
+
+    int i = 0;
     while ((de = readdir(dr)) != NULL) {
         char* name = de->d_name;
-
-        if (has_all != 1 && name[0] == '.') continue;
-
-        printf("%s\n", name);
+        files[i] = name;
+        i++;
     }
+
+    printf("%d\n", i);
+    printf("%d\n", files_len);
   
-    closedir(dr);    
+    closedir(dr);
     
+    qsort(files, files_len, sizeof(char *), cmpstringp);
+
+    for (; i > 0; i--) {
+        printf("%d\n", files_len - i);
+        printf("%s\n", files[files_len - i]);
+        free(files[files_len - i]);
+    }
+
+    free(files);
+    
+
     return (0);
 }
