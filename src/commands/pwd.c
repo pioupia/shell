db50564d7936de2d3432ff1e784e54ea0712a8a8
pioupia <49518790+pioupia@users.noreply.github.com>
2023-02-04 09:50:58 +0100

    Feat: adding clear command and update pwd command (#17)

--- src/commands/pwd.c
+++ src/commands/pwd.c
@@ -1,42 +1,14 @@
-#include <malloc.h>
-#include <unistd.h>
+//
+// Created by pioupia on 02/02/23.
+//
+#include <stdio.h>
 
 extern char **environ;
 
-int my_len(const char *str)
-{
-    int len = 0;
-
-    while(str[len])
-        len++;
-
-    return (len);
-}
-
-char* my_strcpy(const char *str)
-{
-    int len = my_len(str);
-    char *cpy = malloc(sizeof(char*) * (len + 1));
-
-    for (int i = 0; (cpy[i] = str[i]); i++);
-    return (cpy);
-}
-
-void my_putnchar(const char *str) {
-    int len = my_len(str);
-
-    char *string = my_strcpy(str);
-    string[len] = '\n';
-
-    write(STDOUT_FILENO, string, len + 1);
-
-    free(string);
-}
-
 int main(void) {
-    char *pwd = environ[0];
+    if (environ[0] == NULL) return (1);
 
-    my_putnchar(pwd);
+    printf("%s\n", environ[0]);
 
-    return 0;
+    return (0);
 }
