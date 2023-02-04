102e91effaab4f710ec5dacc670b014383bb7506
pioupia <pioupiadeux@gmail.com>
2023-02-04 17:06:29 +0100

    Feat: updating the ls command

--- src/commands/ls.c
+++ src/commands/ls.c
@@ -5,8 +5,11 @@
 #include <dirent.h>
 #include <string.h>
 #include <stdlib.h>
-#include <sys/types.h>
 #include <sys/stat.h>
+#include <unistd.h>
+
+#define IS_FILE         1
+#define IS_EXECUTABLE   2
 
 extern char **environ;
 
@@ -14,7 +17,12 @@ extern char **environ;
 int is_regular_file(const char *path)
 {
     struct stat path_stat;
-    stat(path, &path_stat);
+    int res = stat(path, &path_stat);
+
+    if (res == 0 && access(path, X_OK)) {
+        return (2);
+    }
+
     return (S_ISREG(path_stat.st_mode));
 }
 
@@ -24,7 +32,8 @@ int cmpstringp(const void *p1, const void *p2)
 }
 
 int main(int argc, char **argv) {
-    if (environ[0] == NULL) return (1);
+    char *pwd = environ[0];
+    if (pwd == NULL) return (1);
 
     int has_all = 0;
 
@@ -43,11 +52,9 @@ int main(int argc, char **argv) {
         }
     }
 
-    printf("%d\n", has_all);
-
     struct dirent *de;
   
-    DIR *dr = opendir(environ[0]);
+    DIR *dr = opendir(pwd);
   
     if (dr == NULL)
     {
@@ -62,32 +69,45 @@ int main(int argc, char **argv) {
 
     closedir(dr);
 
-    dr = opendir(environ[0]);
+    dr = opendir(pwd);
 
     char **files = calloc(files_len, sizeof(char *));
 
     int i = 0;
     while ((de = readdir(dr)) != NULL) {
-        char* name = de->d_name;
-        files[i] = name;
+        files[i] = de -> d_name;
+        printf("%s\n", files[i]);
         i++;
     }
 
-    printf("%d\n", i);
-    printf("%d\n", files_len);
-  
     closedir(dr);
     
     qsort(files, files_len, sizeof(char *), cmpstringp);
 
+    strcat(pwd, "/");
+
     for (; i > 0; i--) {
-        printf("%d\n", files_len - i);
-        printf("%s\n", files[files_len - i]);
-        free(files[files_len - i]);
+        char *value = files[files_len - i];
+
+        if (has_all != 1 && value[0] == '.') continue;
+
+        char *temp_pwd = malloc(strlen(pwd));
+        strcpy(temp_pwd, pwd);
+
+        char *file_path = strcat(temp_pwd, value);
+
+        int type = is_regular_file(file_path);
+        if (type == IS_FILE) {
+            printf("%s\n", value);
+        } else if (type == IS_EXECUTABLE) {
+            printf("* %s\n", value);
+        } else {
+            printf("%s/\n", value);
+        }
+
+        free(file_path);
     }
 
     free(files);
-    
-
     return (0);
 }
