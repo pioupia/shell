d958d5fe3d281e376e29984e47a44f63df057747
pioupia <pioupiadeux@gmail.com>
2023-02-08 15:52:18 +0100

    Feat: updating the ls command

--- src/commands/ls.c
+++ src/commands/ls.c
@@ -4,31 +4,32 @@
 #include <stdio.h>
 #include <dirent.h>
 #include <string.h>
-#include <stdlib.h>
 #include <sys/stat.h>
-#include <unistd.h>
+#include <errno.h>
 
 #define IS_FILE         1
 #define IS_EXECUTABLE   2
 
+#define FILE_COLOR                  "\x1b[97m"
+#define EXECUTABLE_FILE_COLOR       "\x1b[93m"
+#define DIRECTORY_COLOR             "\x1b[96m"
+#define COLOR_BOLD                  "\x1b[1m"
+#define COLOR_RESET                 "\x1b[0m"
+
 extern char **environ;
 
 
-int is_regular_file(const char *path)
-{
+int is_regular_file(const char *path) {
     struct stat path_stat;
-    int res = stat(path, &path_stat);
+    stat(path, &path_stat);
 
-    if (res == 0 && access(path, X_OK)) {
+    int res = S_ISREG(path_stat.st_mode);
+
+    if (res == 1 && path_stat.st_mode & S_IXUSR) {
         return (2);
     }
 
-    return (S_ISREG(path_stat.st_mode));
-}
-
-int cmpstringp(const void *p1, const void *p2)
-{
-    return strcmp(*(const char **) p1, *(const char **) p2);
+    return (res);
 }
 
 int main(int argc, char **argv) {
@@ -47,67 +48,39 @@ int main(int argc, char **argv) {
                 if (argument[j] != 'a') continue;
 
                 has_all = 1;
-                break; 
+                break;
             }
         }
     }
 
     struct dirent *de;
-  
+
     DIR *dr = opendir(pwd);
-  
-    if (dr == NULL)
-    {
-        printf("And error has occured.");
+
+    if (!dr) {
+        if (errno = ENOENT) {
+            perror("Directory doesn't exist");
+        } else {
+            printf("An error has occured.");
+        }
         return (1);
     }
-  
-    int files_len = 0;
+
     while ((de = readdir(dr)) != NULL) {
-        files_len++;
-    }
+        char *name = de->d_name;
 
-    closedir(dr);
+        if (has_all != 1 && name[0] == '.') continue;
 
-    dr = opendir(pwd);
-
-    char **files = calloc(files_len, sizeof(char *));
-
-    int i = 0;
-    while ((de = readdir(dr)) != NULL) {
-        files[i] = de -> d_name;
-        printf("%s\n", files[i]);
-        i++;
-    }
-
-    closedir(dr);
-    
-    qsort(files, files_len, sizeof(char *), cmpstringp);
-
-    strcat(pwd, "/");
-
-    for (; i > 0; i--) {
-        char *value = files[files_len - i];
-
-        if (has_all != 1 && value[0] == '.') continue;
-
-        char *temp_pwd = malloc(strlen(pwd));
-        strcpy(temp_pwd, pwd);
-
-        char *file_path = strcat(temp_pwd, value);
-
-        int type = is_regular_file(file_path);
+        int type = is_regular_file(name);
         if (type == IS_FILE) {
-            printf("%s\n", value);
+            printf(FILE_COLOR "%s\n" COLOR_RESET, name);
         } else if (type == IS_EXECUTABLE) {
-            printf("* %s\n", value);
+            printf(EXECUTABLE_FILE_COLOR COLOR_BOLD "%s%s*\n", name, COLOR_RESET);
         } else {
-            printf("%s/\n", value);
+            printf(COLOR_BOLD DIRECTORY_COLOR "%s%s/\n", name, COLOR_RESET);
         }
-
-        free(file_path);
     }
+    closedir(dr);
 
-    free(files);
     return (0);
 }
